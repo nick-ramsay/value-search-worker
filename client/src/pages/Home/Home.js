29adc3e7361c0bfd1ebf89dcdf1a47930ecc873d
Nicholas Ramsay <nramsay@intersystems.com>
2021-03-23 14:24:18 +1100

    Tidied up the layouts, removed remaining Morningstar references

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -8,23 +8,26 @@ const Home = () => {
 
     return (
         <div>
-            <div className="App">
-                <div className="container">
+            <div className="container">
+                <div className="row mb-4">
                     <div className="col-md-12 text-center">
-                        <div className="row mb-2">
-                            <input type="text" placeholder="Enter stock symbol here (example: AAPL)..." className="form-control" id="stockSymbolInput" name="stockSymbolInput" onChange={setStockSymbol} aria-describedby="stockSymbolHelp" />
-                        </div>
-                        <div className="row mb-2">
-                            <button className="btn btn-sm btn-custom">Fetch Stock Data</button>
-                        </div>
+                        <input type="text" placeholder="Enter stock symbol here (example: AAPL)..." className="form-control" id="stockSymbolInput" name="stockSymbolInput" onChange={setStockSymbol} aria-describedby="stockSymbolHelp" />
+                    </div>
+                </div>
+                <div className="row mb-4">
+                    <div className="col-md-12 text-center">
+                        <button className="btn btn-sm btn-custom">Fetch Stock Data</button>
+                    </div>
+                </div>
 
-                        <div className="row">
-                            {stockData !== {} && stockData !== undefined ?
-                                ""
-                                :
-                                <p>No Stock Data</p>
-                            }
-                        </div>
+                <div className="row mt-4">
+                    <div className="col-md-12 text-center">
+                        {Object.keys(stockData).length !== 0 && stockData !== undefined ?
+                            ""
+                            :
+                            <p>No Stock Data</p>
+
+                        }
                     </div>
                 </div>
             </div>
