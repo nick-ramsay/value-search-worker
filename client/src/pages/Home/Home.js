995ac4c62739ae794e61dcb884dffd4390b1bb3e
Nick Ramsay <NickRamsay1989@gmail.com>
2021-04-17 22:18:04 +1000

    Significant progress switching from object mapping table and rendering rows and cells

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -6,29 +6,16 @@ import "./style.css";
 const Home = () => {
     let [stockSymbol, setStockSymbol] = useInput("");
     let [stockData, setStockData] = useState({});
-    let [stockDataArray, setStockDataArray] = useState([]);
-    let currentAppendedRow = 0;
 
     const scrapeQuoteData = (symbol) => {
         setStockData(stockData => Object({}));
-        setStockData(stockDataArray => []);
         API.scrapeQuoteData(symbol).then(res => {
             console.log(res);
             setStockData(stockData => res.data);
-
-            let tempStockDataArray = [];
-            for (let key in res.data) {
-                if (res.data.hasOwnProperty(key)) {
-                    let value = res.data[key];
-                    tempStockDataArray.push([key, value]);
-                }
-            };
-            console.log(tempStockDataArray);
-            setStockDataArray(stockDataArray => tempStockDataArray);
         });
     }
 
-    let currentStockDataRow;
+    let currentStockDataRowArray = [];
 
     useEffect(() => {
     }, [])
@@ -46,8 +33,8 @@ const Home = () => {
                         <button className="btn btn-sm btn-custom" onClick={() => scrapeQuoteData(stockSymbol)}>Fetch Stock Data</button>
                         <div class="accordion mt-2" id="accordionExample">
                             <div>
-                                <a className="mt-4" style={{fontSize: 14}} data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                            Raw Code &#x21c5;
+                                <a className="mt-4" style={{ fontSize: 14 }} data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                    Raw Code &#x21c5;
                                 </a>
                                 <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
                                     <div className="code">
@@ -66,31 +53,52 @@ const Home = () => {
                             <div>
                                 <table className="table table-sm table-borderless table-responsive stock-data-table">
                                     <tbody id="stock-data-table-body">
-                                        {stockDataArray.map((item, itemIndex) => {
-                                            let tableBodyDiv = document.getElementById("stock-data-table-body");
-                                            if (itemIndex % 2 === 0) {
-                                                currentAppendedRow += 1;
-                                                const newRow = document.createElement("tr");
-                                                newRow.setAttribute("id", "stock-data-row" + currentAppendedRow);
-                                                newRow.setAttribute("key", "stock-data-row" + currentAppendedRow);
-                                                tableBodyDiv.appendChild(newRow);
+                                        {Object.keys(stockData).map((keyName, i) => {
+                                            currentStockDataRowArray.push(keyName);
+                                            currentStockDataRowArray.push(stockData[keyName]);
+                                            console.log(currentStockDataRowArray);
+                                            if (i % 3 === 0 && i !== 0) {
+                                                return(
+                                                <tr>
+                                                    <td>{currentStockDataRowArray[0]}</td>
+                                                    <td>{currentStockDataRowArray[1]}</td>
+                                                    <td>{currentStockDataRowArray[2]}</td>
+                                                    <td>{currentStockDataRowArray[3]}</td>
+                                                    <td>{currentStockDataRowArray[4]}</td>
+                                                    <td>{currentStockDataRowArray[5]}</td>
+                                                </tr>)
+                                            };
+                                            if (i % 3 === 0 && i !== 0) {
+                                                currentStockDataRowArray = []
                                             }
-
-                                            let newCell1 = document.createElement("td");
-                                            newCell1.setAttribute("id", "stock-title-cell" + itemIndex);
-                                            newCell1.setAttribute("key", "stock-title-cell" + itemIndex);
-                                            newCell1.setAttribute("class", "stock-data-title-column");
-                                            newCell1.innerText = item[0];
-                                            document.getElementById("stock-data-row" + currentAppendedRow).appendChild(newCell1);
-
-                                            let newCell2 = document.createElement("td");
-                                            newCell2.setAttribute("id", "stock-data-cell" + itemIndex);
-                                            newCell2.setAttribute("key", "stock-data-cell" + itemIndex);
-                                            newCell2.setAttribute("class", "stock-data-column");
-                                            newCell2.innerText = item[1];
-                                            document.getElementById("stock-data-row" + currentAppendedRow).appendChild(newCell2)
-
                                         })
+
+                                            /*stockDataArray.map((item, itemIndex) => {
+                                                let tableBodyDiv = document.getElementById("stock-data-table-body");
+                                                if (itemIndex % 2 === 0) {
+                                                    currentAppendedRow += 1;
+                                                    const newRow = document.createElement("tr");
+                                                    newRow.setAttribute("id", "stock-data-row" + currentAppendedRow);
+                                                    newRow.setAttribute("key", "stock-data-row" + currentAppendedRow);
+                                                    tableBodyDiv.appendChild(newRow);
+                                                }
+    
+                                                let newCell1 = document.createElement("td");
+                                                newCell1.setAttribute("id", "stock-title-cell" + itemIndex);
+                                                newCell1.setAttribute("key", "stock-title-cell" + itemIndex);
+                                                newCell1.setAttribute("class", "stock-data-title-column");
+                                                newCell1.innerText = item[0];
+                                                document.getElementById("stock-data-row" + currentAppendedRow).appendChild(newCell1);
+    
+                                                let newCell2 = document.createElement("td");
+                                                newCell2.setAttribute("id", "stock-data-cell" + itemIndex);
+                                                newCell2.setAttribute("key", "stock-data-cell" + itemIndex);
+                                                newCell2.setAttribute("class", "stock-data-column");
+                                                newCell2.innerText = item[1];
+                                                document.getElementById("stock-data-row" + currentAppendedRow).appendChild(newCell2)
+                                            
+                                            })
+                                            */
                                         }
                                     </tbody>
                                 </table>
