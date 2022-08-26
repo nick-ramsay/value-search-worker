04a55e2d177de76ded39e68670393764a72ab964
Nick Ramsay <NickRamsay1989@gmail.com>
2022-08-26 20:40:09 +1000

    Reforming application to be a backend scraper

--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const searchingFinvizRoutes = require("./searchingFinvizRoutes");
-
-// investment-tracker routes
-router.use("/searching-finviz", searchingFinvizRoutes);
-
-module.exports = router;
--- routes/api/searchingFinvizRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const searchingFinvizControllers = require("../../controllers/searchingFinvizControllers");
-
-router
-  .route("/scrape-quote-data")
-  .post(searchingFinvizControllers.scrapeQuoteData);
-
-module.exports = router;
