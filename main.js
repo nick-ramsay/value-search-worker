82288c2d2de74b13879d1d2a7f4865ebbd31ef18
nick-ramsay <nick.ramsay@datadoghq.com>
2023-01-18 12:03:00 +1100

    Hopefully fixed bug with restarting at end of for loop

--- main.js
+++ main.js
@@ -1,49 +1,60 @@
-const tracer = require('dd-trace').init();
+const tracer = require("dd-trace").init();
 
 let fetchQuote = require("./fetch-quote.js");
 
-require('dotenv').config()
+require("dotenv").config();
 const axios = require("axios");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const db = require("./models");
 
 //Tokens & Keys
 const uri = process.env.MONGO_URI;
 
-const fetchIEXQuote = (currentSymbol, fullSymbolData) => { return fetchQuote(currentSymbol, fullSymbolData) };
+const fetchIEXQuote = (currentSymbol, fullSymbolData) => {
+  return fetchQuote(currentSymbol, fullSymbolData);
+};
 
 const sleep = (milliseconds) => {
-    return new Promise(resolve => setTimeout(resolve, milliseconds))
-}
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
 
 const beginFetching = () => {
-    mongoose.connect(uri)
-        .then(() => {
-            db.StockSymbols.find(
-                {}
-            )
-                .then(async (res) => {
-                    for (let i = 0; res.length; i++) {
-                        let currentTime = Date();
-                        let daydiff = 1000 * 60 * 60 * 24;  
-                        
-                        let quoteLastUpdated = res[i].quoteLastUpdated;
-                        let daysSinceQuoteLastUpdated = (new Date(currentTime).getTime() - new Date(quoteLastUpdated).getTime())/daydiff;
+  mongoose.connect(uri).then(() => {
+    db.StockSymbols.find({})
+      .then(async (res) => {
+        for (let i = 0; res.length > i; i++) {
+          let currentTime = Date();
+          let daydiff = 1000 * 60 * 60 * 24;
 
-                        let fundamentalsLastUpdated = res[i].fundamentalsLastUpdated;
-                        let daysSinceFundamentalsLastUpdated = (new Date(currentTime).getTime() - new Date(fundamentalsLastUpdated).getTime())/daydiff;
-                        
-                        let iexStatsLastUpdated = res[i].iexStatusLastUpdated;
-                        let daysSinceiexStatsLastUpdated = (new Date(currentTime).getTime() - new Date(iexStatsLastUpdated).getTime())/daydiff
+          let quoteLastUpdated = res[i].quoteLastUpdated;
+          let daysSinceQuoteLastUpdated =
+            (new Date(currentTime).getTime() -
+              new Date(quoteLastUpdated).getTime()) /
+            daydiff;
 
-                        let valueSearchScoreLastUpdated = res[i].valueSearchScoreLastUpdated;
-                        let daysSinceValueSearchScoreLastUpdated = (new Date(currentTime).getTime() - new Date(valueSearchScoreLastUpdated).getTime())/daydiff
-                        
-                        let currentSymbol = res[i].symbol
-                        //console.log(currentSymbol);
-                        await sleep(1000);
-                        //console.log(res[i])
-                        /*
+          let fundamentalsLastUpdated = res[i].fundamentalsLastUpdated;
+          let daysSinceFundamentalsLastUpdated =
+            (new Date(currentTime).getTime() -
+              new Date(fundamentalsLastUpdated).getTime()) /
+            daydiff;
+
+          let iexStatsLastUpdated = res[i].iexStatusLastUpdated;
+          let daysSinceiexStatsLastUpdated =
+            (new Date(currentTime).getTime() -
+              new Date(iexStatsLastUpdated).getTime()) /
+            daydiff;
+
+          let valueSearchScoreLastUpdated = res[i].valueSearchScoreLastUpdated;
+          let daysSinceValueSearchScoreLastUpdated =
+            (new Date(currentTime).getTime() -
+              new Date(valueSearchScoreLastUpdated).getTime()) /
+            daydiff;
+
+          let currentSymbol = res[i].symbol;
+          //console.log(currentSymbol);
+          await sleep(1000);
+          //console.log(res[i])
+          /*
                         console.log("⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄" + currentSymbol + "⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄⌄")
                         console.log(currentTime);
                         console.log("Quote: " + quoteLastUpdated);
@@ -57,19 +68,29 @@ const beginFetching = () => {
                         console.log("-----------------------------");
                         */
 
-                        if (daysSinceQuoteLastUpdated >= 1 && daysSinceQuoteLastUpdated !== undefined && isNaN(daysSinceQuoteLastUpdated) === false) {
-                            fetchIEXQuote(currentSymbol, res[i].data);
-                            //console.log("Current Iterator: " + i);
-                        } else if (daysSinceQuoteLastUpdated < 1) {
-                            //console.log("Current Iterator: " + i);
-                            //console.log(res.length)
-                            console.log("⛔️ " + currentSymbol + " quote already up-to-date ⛔️")
-                        }
-                    }
-                    beginFetching();
-                })
-                .catch(err => console.log(err));
-        });
-    }
+          if (
+            daysSinceQuoteLastUpdated >= 1 &&
+            daysSinceQuoteLastUpdated !== undefined &&
+            isNaN(daysSinceQuoteLastUpdated) === false
+          ) {
+            fetchIEXQuote(currentSymbol, res[i].data);
+            //console.log("Current Iterator: " + i);
+          } else if (daysSinceQuoteLastUpdated < 1) {
+            //console.log("Current Iterator: " + i);
+            //console.log(res.length)
+            console.log(
+              "⛔️ " + currentSymbol + " quote already up-to-date ⛔️"
+            );
+          }
+        }
+        start();
+      })
+      .catch((err) => console.log(err));
+  });
+};
 
-    beginFetching();
+const start = () => {
+  beginFetching();
+};
+
+start();
