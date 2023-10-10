9435fd5ce32c8972ddad6e49c0ae83fb67bc7995
Nick Ramsay <NickRamsay1989@gmail.com>
2023-10-10 21:23:32 +1100

    Created portfolio-conversion script and ran script to convert portfolio array to portfolioItems documents

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 module.exports = {
+    Accounts: require("./Accounts"),
     StockSymbols: require("./StockSymbols"),
     StockData: require("./StockData"),
+    Portfolio: require("./Portfolio"),
     PortfolioItems: require("./PortfolioItems")
   };
   
